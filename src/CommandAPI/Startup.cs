13fdaf9bf802414947a76edd5d440a747ae9c0fd
Vadim.P <vadim.p@cmstuning.com>
2021-02-22 17:57:46 +0300

    + CommandAPI.Data

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CommandAPI.Data;
 
 namespace CommandAPI
 {
@@ -18,6 +19,8 @@ namespace CommandAPI
         {
              /*********/
              services.AddControllers();
+
+             services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
 
         }
 
