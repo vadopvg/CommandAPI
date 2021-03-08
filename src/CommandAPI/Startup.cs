37f0851dcd3cce5f4e5cb902204ad4b657cd2df0
Vadim.P.N <vadim.p@cmstuning.com>
2021-03-09 02:59:08 +0300

    add UserSecret

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using CommandAPI.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace CommandAPI
 {
@@ -25,8 +26,15 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
-                (Configuration.GetConnectionString("PostgreSqlConnection")));
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString =  Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+
+            services.AddDbContext<CommandContext> (opt => opt.UseNpgsql(builder.ConnectionString));
+
+           // services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
+           //     (Configuration.GetConnectionString("PostgreSqlConnection")));
 
             /*********/
             services.AddControllers();
